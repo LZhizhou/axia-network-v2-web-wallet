fce494513597893016b2f0b0dc9c23d51bf69499
NamanPhagiwala <namanphagiwala21@gmail.com>
2022-06-27 15:42:16 +0530

    UI CHANGES ZVR-141

--- src/components/wallet/Sidebar.vue
+++ src/components/wallet/Sidebar.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="wallet_sidebar">
+        <ConfirmLogout ref="logout"></ConfirmLogout>
         <div class="stick">
             <div class="brand">
                 <img v-if="$root.theme === 'day'" src="@/assets/logo.png" />
@@ -7,12 +8,12 @@
             </div>
             <div class="links">
                 <router-link to="/wallet" class="wallet_link">
-                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/portfolio_nav.png" />
+                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/portfolio_nav.svg" />
                     <img v-else src="@/assets/sidebar/portfolio_nav_night.png" />
                     {{ $t('wallet.sidebar.portfolio') }}
                 </router-link>
                 <router-link to="/wallet/transfer" data-cy="wallet_transfer" class="wallet_link">
-                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/transfer_nav.png" />
+                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/transfer_nav.svg" />
                     <img v-else src="@/assets/sidebar/transfer_nav.png" />
                     {{ $t('wallet.sidebar.send') }}
                 </router-link>
@@ -21,43 +22,41 @@
                     data-cy="wallet_export"
                     class="wallet_export wallet_link"
                 >
-                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/crosschain_nav.png" />
+                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/crosschain_nav.svg" />
                     <img v-else src="@/assets/sidebar/crosschain_nav.png" />
                     {{ $t('wallet.sidebar.export') }}
                 </router-link>
                 <router-link to="/wallet/earn" data-cy="wallet_earn" class="wallet_link">
-                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/earn_nav.png" />
+                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/earn_nav.svg" />
                     <img v-else src="@/assets/sidebar/earn_nav.png" />
                     {{ $t('wallet.sidebar.earn') }}
                 </router-link>
                 <router-link to="/wallet/studio" data-cy="wallet_studio" class="wallet_link">
-                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/studio_nav.png" />
+                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/studio_nav.svg" />
                     <img v-else src="@/assets/sidebar/studio_nav.png" />
                     {{ $t('wallet.sidebar.studio') }}
                 </router-link>
                 <router-link to="/wallet/activity" data-cy="wallet_activity" class="wallet_link">
-                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/activity_nav.png" />
+                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/activity_nav.svg" />
                     <img v-else src="@/assets/sidebar/activity_nav.png" />
                     {{ $t('wallet.sidebar.activity') }}
                 </router-link>
                 <router-link to="/wallet/keys" data-cy="wallet_manage" class="wallet_link">
-                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/manage_nav.png" />
-                    <img v-else src="@/assets/sidebar/manage_nav.png" />
+                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/manage_nav.svg" />
+                    <img v-else src="@/assets/sidebar/manage_nav.svg" />
                     {{ $t('wallet.sidebar.manage') }}
                 </router-link>
                 <router-link to="/wallet/advanced" data-cy="wallet_advanced" class="wallet_link">
-                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/advanced_nav.png" />
-                    <img v-else src="@/assets/sidebar/advanced_nav.png" />
+                    <img v-if="$root.theme === 'day'" src="@/assets/sidebar/advanced_nav.svg" />
                     {{ $t('wallet.sidebar.advanced') }}
                 </router-link>
             </div>
             <div class="bottom">
                 <AccountMenu class="wallet_link"></AccountMenu>
                 <LanguageSelect></LanguageSelect>
-                <div>
-                    <button @click="logout" class="logout">
-                        {{ $t('logout.button') }}
-                    </button>
+                <div @click="logout" class="logout">
+                    <img src="@/assets/sidebar/logout.svg" />
+                    <div>{{ $t('logout.button') }}</div>
                 </div>
             </div>
         </div>
@@ -73,6 +72,12 @@ export default {
         AccountMenu,
         LanguageSelect,
         ConfirmLogout,
+    },
+    methods: {
+        logout() {
+            // @ts-ignore
+            this.$refs.logout.open()
+        },
     },
 }
 </script>
@@ -131,9 +136,9 @@ export default {
             opacity: 1;
             background: #e9f6ff;
             font-weight: 500;
-
             img {
-                opacity: 1;
+                filter: invert(46%) sepia(72%) saturate(816%) hue-rotate(166deg) brightness(92%)
+                    contrast(94%);
             }
         }
 
@@ -142,6 +147,12 @@ export default {
             margin-right: 15px;
             object-fit: contain;
         }
+    }
+    .logout {
+        height: 20px;
+        margin-right: 1px;
+        display: flex;
+        column-gap: 2px;
     }
 }
 .wallet_export {
